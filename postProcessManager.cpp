f1187ba0268ce0d27481e67ffaf2e2e0e8d47666
NotNallath <NotNallath@example.com>
2013-11-30 00:27:42 +0100

    Make sure the backbuffer is a power of 2, which is required on some systems.

--- postProcessManager.cpp
+++ postProcessManager.cpp
@@ -13,6 +13,18 @@ void PostProcessorManager::triggerPostProcess(const char* name, float value)
     postProcessorMap[name]->trigger(value);
 }
 
+int powerOfTwo(int v)
+{
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v;
+}
+
 sf::RenderTarget& PostProcessorManager::getPrimaryRenderTarget(sf::RenderTarget& window)
 {
     for(std::map<sf::String, PostProcessor*>::iterator it = postProcessorMap.begin(); it != postProcessorMap.end(); it++)
@@ -22,11 +34,15 @@ sf::RenderTarget& PostProcessorManager::getPrimaryRenderTarget(sf::RenderTarget&
             if (backBuffer.getSize().x < 1)
             {
                 //Setup a backBuffer to render the game on. Then we can render the backbuffer back to the main screen with full-screen shader effects
-                float w = window.getView().getViewport().width * window.getSize().x;
-                float h = window.getView().getViewport().height * window.getSize().y;
-                backBuffer.create(w, h, false);
+                int w = window.getView().getViewport().width * window.getSize().x;
+                int h = window.getView().getViewport().height * window.getSize().y;
+                int tw = powerOfTwo(w);
+                int th = powerOfTwo(h);
+                backBuffer.create(tw, th, false);
                 backBuffer.setRepeated(true);
-                backBuffer.setView(sf::View(sf::Vector2f(160,120), sf::Vector2f(320, 240)));
+                sf::View view(sf::Vector2f(160,120), sf::Vector2f(320, 240));
+                view.setViewport(sf::FloatRect(0, 0, float(w) / float(tw), float(h) / float(th)));
+                backBuffer.setView(view);
             }
             return backBuffer;
         }
@@ -41,8 +57,8 @@ void PostProcessorManager::postProcessRendering(sf::RenderTarget& window)
         if (it->second->active())
         {
             backBuffer.display();
-            sf::Sprite backBufferSprite(backBuffer.getTexture());
-            backBufferSprite.setScale(320.0/backBuffer.getSize().x, 240.0/backBuffer.getSize().y);
+            sf::Sprite backBufferSprite(backBuffer.getTexture(), sf::IntRect(0,0, window.getSize().x, window.getSize().y));
+            backBufferSprite.setScale(320.0/window.getSize().x, 240.0/window.getSize().y);
             it->second->shader.setParameter("value", it->second->value);
             window.draw(backBufferSprite, &it->second->shader);
         }
