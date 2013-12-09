8ff7c819e0550ce8f2183e1d3d9d1ee880d6bd3a
NotNallath <NotNallath@example.com>
2013-12-09 09:34:32 +0100

    Put the window creation and handling into a windowManager class.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,5 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <cstdio>
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
-
-sf::Clock Clock;
-
-#include "vectorUtils.h"
-#include "textDraw.h"
-#include "curve.h"
-#include "background.h"
-#include "gameEntity.h"
-#include "textureManager.h"
-#include "scoreManager.h"
-#include "postProcessManager.h"
 #include "windowManager.h"
-
-#include "explosion.h"
-#include "bullet.h"
-#include "player.h"
-
-#include "EnemyGroup.h"
-#include "BasicEnemy.h"
-#include "BurstShotEnemy.h"
-
-#include "bre.h"
-
-#include "Updatable.h"
-#include "Renderable.h"
+#include "background.h"
 #include "MainMenu.h"
 
 int main()
