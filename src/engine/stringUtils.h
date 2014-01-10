f41d691e2ddf4dfd5653ac53fde391cdd673b309
NotNallath <NotNallath@example.com>
2014-01-10 15:59:09 +0100

    Add stringUtils.h

---
+++ src/engine/stringUtils.h
@@ -0,0 +1,30 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Utility function in assisting to convert numbers to strings */
+static inline std::string to_string(int number)
+{
+    char buf[16];
+    itoa(number, buf, 10);
+    return std::string(buf);
+}
+
+static inline std::string to_string(unsigned int number)
+{
+    char buf[16];
+    itoa(number, buf, 10);
+    return std::string(buf);
+}
+
+static inline std::string to_string(double number)
+{
+    char buf[16];
+    sprintf(buf, "%.2f", number);
+    return std::string(buf);
+}
+
+#endif//STRING_UTILS_H
