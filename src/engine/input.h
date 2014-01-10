9337be787d5f89fd9276e5d34e8d95138036ef29
NotNallath <NotNallath@example.com>
2014-01-10 15:50:06 +0100

    Add missing input files.

---
+++ src/engine/input.h
@@ -0,0 +1,22 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "windowManager.h"
+#include "Updatable.h"
+
+class InputHandler : public Updatable
+{
+    P<WindowManager> windowManager;
+
+    sf::Vector2f mousePos;
+    bool mouseButtonDown[sf::Mouse::ButtonCount];
+    bool mouseButtonPressed[sf::Mouse::ButtonCount];
+public:
+    virtual void update(float delta);
+    
+    sf::Vector2f getMousePos() { return mousePos; }
+    bool mouseIsDown(int button) { return mouseButtonDown[button]; }
+    bool mouseIsPressed(int button) { return mouseButtonPressed[button]; }
+};
+
+#endif//INPUT_H
