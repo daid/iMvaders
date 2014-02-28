e0d2f1cec6e65a238250f117237f3988ba5e29c1
NotNallath <NotNallath@example.com>
2014-02-28 18:54:07 +0100

    Add line collision type.

--- src/engine/Collisionable.cpp
+++ src/engine/Collisionable.cpp
@@ -6,6 +6,14 @@ PVector<Collisionable> collisionableList;
 Collisionable::Collisionable(float radius)
 {
     collisionRadius = radius;
+    collisionType = CT_Circle;
+    collisionableList.push_back(this);
+}
+
+Collisionable::Collisionable(sf::Vector2f lineVector)
+{
+    collisionLineVector = lineVector;
+    collisionType = CT_Line;
     collisionableList.push_back(this);
 }
 
@@ -16,6 +24,13 @@ Collisionable::~Collisionable()
 void Collisionable::setCollisionRadius(float radius)
 {
     collisionRadius = radius;
+    collisionType = CT_Circle;
+}
+
+void Collisionable::setCollisionLineVector(sf::Vector2f lineVector)
+{
+    collisionLineVector = lineVector;
+    collisionType = CT_Line;
 }
 
 void Collisionable::collision(Collisionable* target)
@@ -40,15 +55,40 @@ void Collisionable::handleCollisions()
             P<Collisionable> c2 = collisionableList[n];
             if (c2 && c1 != c2)
             {
-                if ((c1->position - c2->position) <= c1->collisionRadius + c2->collisionRadius)
+                if (c1->collisionType == CT_Circle && c2->collisionType == CT_Circle)
                 {
-                    //Get normal pointers from the two collision objects, this to make sure both collision calls have valid object pointers,
-                    //  as the P<> pointers can get cleared if the collision of c1 with c2 destroys c1
-                    Collisionable* pc1 = *c1;
-                    Collisionable* pc2 = *c2;
-                    //Call the virtual collision functions to handle collision.
-                    pc1->collision(pc2);
-                    pc2->collision(pc1);
+                    if ((c1->position - c2->position) <= c1->collisionRadius + c2->collisionRadius)
+                    {
+                        //Get normal pointers from the two collision objects, this to make sure both collision calls have valid object pointers,
+                        //  as the P<> pointers can get cleared if the collision of c1 with c2 destroys c1
+                        Collisionable* pc1 = *c1;
+                        Collisionable* pc2 = *c2;
+                        //Call the virtual collision functions to handle collision.
+                        pc1->collision(pc2);
+                        pc2->collision(pc1);
+                    }
+                }else if (c1->collisionType == CT_Line && c2->collisionType == CT_Circle)
+                {
+					float length = sf::length(c1->collisionLineVector);
+					if (length == 0)
+						continue;
+					sf::Vector2f diff = c2->position - c1->position;
+					float off = (c1->collisionLineVector.x * diff.x + c1->collisionLineVector.y * diff.y) / length;
+					if (off < 0)
+						off = 0;
+					if (off > length)
+						off = length;
+					sf::Vector2f q = c1->position + c1->collisionLineVector/length * off;
+					if (sf::length(q - c2->position) < c2->collisionRadius)
+					{
+                        //Get normal pointers from the two collision objects, this to make sure both collision calls have valid object pointers,
+                        //  as the P<> pointers can get cleared if the collision of c1 with c2 destroys c1
+                        Collisionable* pc1 = *c1;
+                        Collisionable* pc2 = *c2;
+                        //Call the virtual collision functions to handle collision.
+                        pc1->collision(pc2);
+                        pc2->collision(pc1);
+                    }
                 }
             }
             if (!c1)
--- src/engine/Collisionable.h
+++ src/engine/Collisionable.h
@@ -5,17 +5,27 @@
 
 class Collisionable;
 extern PVector<Collisionable> collisionableList;
+enum ECollisionType
+{
+    CT_Circle,
+    CT_Line
+};
+
 class Collisionable: public virtual PObject
 {
 private:
     sf::Vector2f position;
+    sf::Vector2f collisionLineVector;
+    ECollisionType collisionType;
     float collisionRadius;
 public:
     Collisionable(float radius);
+    Collisionable(sf::Vector2f lineVector);
     virtual ~Collisionable();
     virtual void collision(Collisionable* target);
     
     void setCollisionRadius(float radius);
+    void setCollisionLineVector(sf::Vector2f lineVector);
     void setPosition(sf::Vector2f v);
     sf::Vector2f getPosition();
 
